90ca488fcdb3956b73c3f7f3ed9c50fd7827b4ea
eiu165 <eiu165@gmail.com>
2014-11-03 23:19:42 -0600

    first commit

---
+++ Source/FirstCareFit.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FirstCareFit.Web.Startup))]
+namespace FirstCareFit.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
