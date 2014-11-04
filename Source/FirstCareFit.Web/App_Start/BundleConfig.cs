5696fc050d3829089a6ad00632ce7185753c0905
eiu165 <eiu165@gmail.com>
2014-11-03 23:37:09 -0600

    added bootstrap.scss

--- Source/FirstCareFit.Web/App_Start/BundleConfig.cs
+++ Source/FirstCareFit.Web/App_Start/BundleConfig.cs
@@ -24,8 +24,8 @@ namespace FirstCareFit.Web
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Styles/Vendor/Bootstrap/bootstrap.css",
+                      "~/Content/Styles/site.css"));
 
             // Set EnableOptimizations to false for debugging. For more information,
             // visit http://go.microsoft.com/fwlink/?LinkId=301862
